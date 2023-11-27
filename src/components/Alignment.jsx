761c47d32342838e531be4779886ec9a6b3ed692
naz-0 <102573129+naz-0@users.noreply.github.com>
2023-11-26 17:25:25 -0800

    Add files via upload

---
+++ src/components/Alignment.jsx
@@ -0,0 +1,12 @@
+const Alignment = () => {
+    return (
+      <div className="mt-4">
+        <div className="fs-3">
+          <strong> No data yet - select another option </strong>
+          
+        </div>
+      </div>
+    );
+  };
+  
+  export default Alignment;
---
+++ src/components/Surface.jsx
@@ -0,0 +1,11 @@
+const Surface = () => {
+    return (
+      <div className="mt-4">
+        <div className="fs-3">
+          <strong>No data yet - select another option </strong> 
+        </div>
+      </div>
+    );
+  };
+  
+  export default Surface;
