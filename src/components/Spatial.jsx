475a660387c27ab0d546f7a88727f0eda7fdcace
naz-0 <102573129+naz-0@users.noreply.github.com>
2023-12-05 10:26:39 -0800

    add original files

---
+++ src/components/Spatial.jsx
@@ -0,0 +1,11 @@
+const Spatial = () => {
+    return (
+      <div className="mt-4">
+        <div className="fs-3">
+          <strong>No data yet - select another option </strong>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Spatial;
