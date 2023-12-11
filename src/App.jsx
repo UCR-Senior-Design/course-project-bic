1d682ad5aed32e7869e6d17edf253297ee97daa6
Brenda Lorenzo <blore005@ucr.edu>
2023-12-11 08:24:52 -0800

    diplayed 5 types of images

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
 import { useEffect, useState } from "react";
 import reactLogo from './assets/react.svg';
 import './App.css';
-import Brain from "./components/Brain";
-import Spatial from "./components/Spatial";
-import Surface from "./components/Surface";
-import Alignment from "./components/Alignment";
-import Mask from "./components/Mask";
+import Brain from "./components/desc_reconall_T1w";
+import Spatial from "./components/task-commpost_run-01_desc-carpetplot_bold";
+import Surface from "./components/task-commpost_run-01_desc-compcorvar_bold";
+import Alignment from "./components/space-MNI152NLin2009cAsym_T1w";
+import Mask from "./components/dseg";
 // import Plot from "./components/Plot";
 
 export default function App() {
   const [data, setData] = useState("selectTheData");
 
-  const[brainContentVisible, setBrainContentVisible] = useState(false);
-  const[spatialContentVisible, setSpatialContentVisible] = useState(false);
-  const[surfaceContentVisible, setSurfaceContentVisible] = useState(false);
-  const[alignmentContentVisible, setAlignmentContentVisible] = useState(false);
-  const[maskContentVisible, setMaskContentVisible] = useState(false);
+  const [brainContentVisible, setBrainContentVisible] = useState(false);
+  const [spatialContentVisible, setSpatialContentVisible] = useState(false);
+  const [surfaceContentVisible, setSurfaceContentVisible] = useState(false);
+  const [alignmentContentVisible, setAlignmentContentVisible] = useState(false);
+  const [maskContentVisible, setMaskContentVisible] = useState(false);
 
   useEffect(() => {
-    data === "brain mask and brain tissue segmentation of the T1w"
-      ? setBrainContentVisible(true)
-      : setBrainContentVisible(false);
-    data === "spatial normalization of the anatomical T1w reference" ? setSpatialContentVisible(true) : setSpatialContentVisible(false);
-    data === "surface reconstruction" ? setSurfaceContentVisible(true) : setSurfaceContentVisible(false);
-    data === "alignment of functional and anatomical MRI data (surface driven)" ? setAlignmentContentVisible(true) : setAlignmentContentVisible(false);
-    data === "brain mask and (anatomical/crown/temporal) CompCor ROIs" ? setMaskContentVisible(true) : setMaskContentVisible(false);
+    setBrainContentVisible(data === "desc_reconall_T1w");
+    setSpatialContentVisible(data === "task-commpost_run-01_desc-carpetplot_bold");
+    setSurfaceContentVisible(data === "task-commpost_run-01_desc-compcorvar_bold");
+    setAlignmentContentVisible(data === "space-MNI152NLin2009cAsym_T1w");
+    setMaskContentVisible(data === "dseg");
   }, [data]);
-  
+
   const handleOnChange = (e) => {
     setData(e.target.value);
   };
@@ -36,11 +34,7 @@ export default function App() {
   };
 
   const renderResult = () => {
-    let result;
-    data === "selectTheData"
-      ? (result = "fMRIPrep Data Outputs")
-      : (result = makeFirstLetterCapital(data));
-    return result;
+    return data === "selectTheData" ? "fMRIPrep Data Outputs" : makeFirstLetterCapital(data);
   };
 
   return (
@@ -49,22 +43,13 @@ export default function App() {
         <h1> {renderResult()}</h1>
       </div>
       <div className="mt-4">
-        <select className="form-select" value={data} onChange={handleOnChange}> 
-        <option value="selectTheData">Select your choice of data</option>
-        <option value="brain mask and brain tissue segmentation of the T1w">
-          Brain mask and brain tissue segmentation of the T1w
-        </option>
-        <option value="spatial normalization of the anatomical T1w reference">
-          Spatial normalization of the anatomical T1w reference
-        </option>
-        <option value="surface reconstruction">Surface reconstruction</option>
-        <option value="alignment of functional and anatomical MRI data (surface driven)">
-          Alignment of functional and anatomical MRI data (surface driven)
-        </option>
-        <option value="brain mask and (anatomical/crown/temporal) CompCor ROIs">
-          Brain mask and (anatomical/crown/temporal) CompCor ROIs
-        </option>
-
+        <select className="form-select" value={data} onChange={handleOnChange}>
+          <option value="selectTheData">Select your choice of data</option>
+          <option value="desc_reconall_T1w">desc_reconall_T1w</option>
+          <option value="task-commpost_run-01_desc-carpetplot_bold">task-commpost_run-01_desc-carpetplot_bold</option>
+          <option value="task-commpost_run-01_desc-compcorvar_bold">task-commpost_run-01_desc-compcorvar_bold</option>
+          <option value="space-MNI152NLin2009cAsym_T1w">space-MNI152NLin2009cAsym_T1w</option>
+          <option value="dseg">dseg</option>
         </select>
       </div>
       {brainContentVisible && <Brain />}
@@ -72,9 +57,6 @@ export default function App() {
       {surfaceContentVisible && <Surface />}
       {alignmentContentVisible && <Alignment />}
       {maskContentVisible && <Mask />}
-
     </div>
   );
-  }
-
- 
+}
