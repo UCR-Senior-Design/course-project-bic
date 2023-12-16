6d07add2ecf5ea85a2895c18afada78224a55391
cpascx <cpasc012@ucr.edu>
2023-12-15 23:37:48 -0800

    Requirements Update

--- flask_backend/app/visualizations2.py
+++ flask_backend/app/visualizations2.py
@@ -12,6 +12,8 @@ class Visualizations:
         plt.plot(self.data['rot_y'], label='rot_y')
         plt.plot(self.data['rot_z'], label='rot_z')
         plt.title('Rotations Plot')
+        plt.xlabel('Time')  # Assuming time is the x-axis
+        plt.ylabel('Rotation Value')  # General y-axis label
         plt.legend()
         plt.show()
 
@@ -19,6 +21,8 @@ class Visualizations:
         plt.plot(self.data['trans_x'], label='trans_x')
         plt.plot(self.data['trans_y'], label='trans_y')
         plt.plot(self.data['trans_z'], label='trans_z')
+        plt.xlabel('Time')  # Assuming time is the x-axis
+        plt.ylabel('Translation Value')  # General y-axis label
         plt.title('Translations Plot')
         plt.legend()
         plt.show()
@@ -26,5 +30,7 @@ class Visualizations:
     def plot_framewise(self):
         plt.plot(self.data['framewise_displacement'], label='framewise_displacement')
         plt.title('Framewise Displacement Plot')
+        plt.xlabel('Time')  # Assuming time is the x-axis
+        plt.ylabel('Framewise')  # General y-axis label
         plt.legend()
         plt.show()
