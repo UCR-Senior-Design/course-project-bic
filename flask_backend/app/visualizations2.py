21a8c53148a9edcafe94523f22270b5db0eaf04d
cpascx <cpasc012@ucr.edu>
2023-12-15 23:30:42 -0800

    Revert "final changes"
    This reverts commit 63ad2105316075015b6bc15da9807ca964e6781e.

--- flask_backend/app/visualizations2.py
+++ flask_backend/app/visualizations2.py
@@ -12,8 +12,6 @@ class Visualizations:
         plt.plot(self.data['rot_y'], label='rot_y')
         plt.plot(self.data['rot_z'], label='rot_z')
         plt.title('Rotations Plot')
-        plt.xlabel('Time')  # Assuming time is the x-axis
-        plt.ylabel('Rotation Value')  # General y-axis label
         plt.legend()
         plt.show()
 
@@ -21,8 +19,6 @@ class Visualizations:
         plt.plot(self.data['trans_x'], label='trans_x')
         plt.plot(self.data['trans_y'], label='trans_y')
         plt.plot(self.data['trans_z'], label='trans_z')
-        plt.xlabel('Time')  # Assuming time is the x-axis
-        plt.ylabel('Translation Value')  # General y-axis label
         plt.title('Translations Plot')
         plt.legend()
         plt.show()
@@ -30,7 +26,5 @@ class Visualizations:
     def plot_framewise(self):
         plt.plot(self.data['framewise_displacement'], label='framewise_displacement')
         plt.title('Framewise Displacement Plot')
-        plt.xlabel('Time')  # Assuming time is the x-axis
-        plt.ylabel('Framewise')  # General y-axis label
         plt.legend()
         plt.show()
--- flask_backend/react_frontend/src/components/Visualization_Component.jsx
+++ flask_backend/react_frontend/src/components/Visualization_Component.jsx
@@ -25,29 +25,21 @@ const VisualizationComponent = () => {
       <button onClick={handleGeneratePlots}>Generate Plots</button>
 
       <div>
-      {plots.rotations && (
-          <h2>Rotations Plot</h2>
-        )}
-     
+        <h2>Rotations Plot</h2>
         {plots.rotations && (
           <img src={rot} alt="Rotations Plot" />
         )}
       </div>
 
       <div>
-      {plots.translations && (
-          <h2>Translations Plot</h2>
-        )}
-        
+        <h2>Translations Plot</h2>
         {plots.translations && (
           <img src={trans} alt="Translations Plot" />
         )}
       </div>
 
       <div>
-      {plots.framewise&& (
         <h2>Framewise Displacement Plot</h2>
-        )}
         {plots.framewise && (
           <img src={frame} alt="Framewise Displacement Plot" />
         )}
