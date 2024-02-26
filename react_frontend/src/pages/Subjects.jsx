7dce73d1cbe2a1924f07a78e3a14495b506ac5ef
inaz001 <inaz001@ballard-lab-processer>
2024-02-26 06:16:09 +0000

    grouping sub, run# in figures

--- react_frontend/src/pages/Subjects.jsx
+++ react_frontend/src/pages/Subjects.jsx
@@ -26,22 +26,42 @@ const Subjects = () => {
         console.error('Error fetching SVG paths:', error);
         setError('Failed to fetch SVG paths. Please try again later.'); // Set error message
       });
-  }, [subjectFolder]); 
+  }, [subjectFolder]);
+
+  // Function to group SVG paths by task_type
+  const groupByTaskType = () => {
+    return svgPaths.reduce((acc, svgPath) => {
+      const { path, task_type } = svgPath;
+      if (!acc[task_type]) {
+        acc[task_type] = [];
+      }
+      acc[task_type].push(path);
+      return acc;
+    }, {});
+  };
 
   return (
-    <div> 
+    <div>
+      <div className="subject-title-container"> {/* Container for subject title */}
+        <h1 className="subject-title">{subjectTitle}</h1> {/* Display subject folder title */}
+      </div>
       <Container className="mt-5">
         {error && <Alert variant="danger">{error}</Alert>} {/* Display error message */}
-        <h1>{subjectTitle}</h1> {/* Display subject folder title */}
-        {svgPaths.map((path, index) => (
-          <div key={index} className='subjects'>
-            <img 
-              src={`${baseURL}/${path}`} 
-              alt="" 
-              className='img-fluid' 
-              style={{ width: `800px` }} 
-            />
-            <p>{path.split('/').pop()}</p> {/* Display image name as label */}
+
+        {Object.entries(groupByTaskType()).map(([taskType, paths], index) => (
+          <div key={index}>
+            <h2>{taskType}</h2> {/* Render subheader with taskType */}
+            {paths.map((path, index) => (
+              <div key={index} className='subjects'>
+                <img
+                  src={`${baseURL}/${path}`}
+                  alt=""
+                  className='img-fluid'
+                  style={{ width: `800px` }}
+                />
+                <p>{path.split('/').pop()} </p> {/* Display image name as label */}
+              </div>
+            ))}
           </div>
         ))}
       </Container>
