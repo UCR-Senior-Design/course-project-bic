d073ecdd6f0c1ee0e18357dc7990d24c2377ff2c
Brenda <blore005@ucr.edu>
2024-02-28 19:13:37 +0000

    changes to node_modules

--- run.sh
+++ run.sh
@@ -1,17 +1,5 @@
 # #!/bin/bash
 
-# # Run AccessImages.py in the background
-# python /home/blore005/course-project-bic/flask_backend/app/AccessImages.py &
-
-# # Run images.py in the background
-# python /home/blore005/course-project-bic/flask_backend/app/images.py &
-
-# # Run npm dev server
-# cd /home/blore005/course-project-bic/react_frontend
-# npm run dev
-
-# #!/bin/bash
-
 # # Function to handle interrupt signal
 # cleanup() {
 #     echo "Exiting..."
@@ -23,11 +11,12 @@
 # # Trap interrupt signal (Ctrl+C)
 # trap cleanup SIGINT
 
-# # Run AccessImages.py in the background
-# python /home/blore005/course-project-bic/flask_backend/app/AccessImages.py &
+# # Run http server to serve images
+# cd /home/blore005/data/derivatives
+# python -m http.server 8080 &
 
 # # Store the PID of the background process
-# pid_access_images=$!
+# pid_http_server=$!
 
 # # Run images.py in the background
 # python /home/blore005/course-project-bic/flask_backend/app/images.py &
@@ -39,7 +28,7 @@
 # cd /home/blore005/course-project-bic/react_frontend
 # npm run dev
 
-# # If npm run dev exits, kill the background Python processes
+# # If npm run dev exits, kill the background processes
 # cleanup
 
 #!/bin/bash
@@ -47,8 +36,8 @@
 # Function to handle interrupt signal
 cleanup() {
     echo "Exiting..."
-    # Kill the background Python processes
-    pkill -P $$  # Kill processes started by this script
+    # Kill the background processes
+    kill "$pid_http_server" "$pid_images" >/dev/null 2>&1
     exit 1
 }
 
@@ -57,13 +46,13 @@ trap cleanup SIGINT
 
 # Run http server to serve images
 cd /home/blore005/data/derivatives
-python -m http.server 8080 &
+python3 -m http.server --bind 127.0.0.1 8080 &
 
 # Store the PID of the background process
 pid_http_server=$!
 
 # Run images.py in the background
-python /home/blore005/course-project-bic/flask_backend/app/images.py &
+python3 /home/blore005/course-project-bic/flask_backend/app/images.py &
 
 # Store the PID of the background process
 pid_images=$!
