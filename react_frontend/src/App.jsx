b5e2cf79e3d2d01b234b1d107a40eb3149cb6d8d
Brenda <blore005@ucr.edu>
2024-02-29 10:17:29 +0000

    added feature to input dataset

--- react_frontend/src/App.jsx
+++ react_frontend/src/App.jsx
@@ -1,28 +1,61 @@
+// import './App.css';
+// import Sidebar from './components/Sidebar';
+// import Header from './components/Header';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Subjects from './pages/Subjects';
+// import Figures from './pages/Figures';
+// import SubjectView from './pages/PlotSubjectView';
+// import TypeView from './pages/PlotType';
+// import Welcome from "./pages/Welcome";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Header />
+//       {/* <Sidebar /> */}
+//       <Routes>
+//         <Route path='/' element={<Welcome />} /> 
+//         <Route path='/welcome' element={<Welcome />} /> 
+//         <Route path='/home' element={<Home />} /> 
+//         <Route path='/about' element={<About />} /> 
+//         <Route path='/subjects/:subjectFolder' element={<Subjects />} /> 
+//         <Route path='/Figures/:task/:taskName' element={<Figures />} />
+//         <Route path='/plots/subjects/:subjectFolder' element={<SubjectView />} />
+//         <Route path='/plots/type/:plotType' element={<TypeView />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
-import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Welcome from "./pages/Welcome";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./Layout"; // Import the layout component here
 import Subjects from './pages/Subjects';
 import Figures from './pages/Figures';
 import SubjectView from './pages/PlotSubjectView';
 import TypeView from './pages/PlotType';
-import Welcome from "./pages/Welcome";
-import Home from "./pages/Home";
-import About from "./pages/About";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Sidebar />
       <Routes>
-        <Route path='/welcome' element={<Welcome />} /> 
-        <Route path='/home' element={<Home />} /> 
-        <Route path='/about' element={<About />} /> 
-        <Route path='/subjects/:subjectFolder' element={<Subjects />} /> 
-        <Route path='/Figures/:task/:taskName' element={<Figures />} />
-        <Route path='/plots/subjects/:subjectFolder' element={<SubjectView />} />
-        <Route path='/plots/type/:plotType' element={<TypeView />} />
+        <Route path='/' element={<Welcome />} />
+        <Route path='/welcome' element={<Welcome />} />
+        <Route path='/home' element={<Layout><Home /></Layout>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/subjects/:subjectFolder' element={<Layout><Subjects /></Layout>} />
+        <Route path='/Figures/:task/:taskName' element={<Layout><Figures /></Layout>} />
+        <Route path='/plots/subjects/:subjectFolder' element={<Layout><SubjectView /></Layout>} />
+        <Route path='/plots/type/:plotType' element={<Layout><TypeView /></Layout>} />
       </Routes>
     </Router>
   );
--- react_frontend/src/pages/Welcome.jsx
+++ react_frontend/src/pages/Welcome.jsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios'; 
 
-function Welcome() {
-  return (
-    <div className="welcome" style={{ color: "#7785ac" }}>
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Welcome!</h1>
-            <p>
-            {/* <u><strong></strong> </u><br /><br /> */}
-            </p>
-          </div>
+const DataPathForm = () => {
+    const [dataPath, setDataPath] = useState(''); // State variable to store the data path entered by the user
+    const [message, setMessage] = useState(''); // State variable to store success message from the server
+    const [error, setError] = useState(''); // State variable to store error message from the server
+
+    // Function to update the dataPath state variable as the user types in the input field
+    const handleInputChange = (event) => {
+        setDataPath(event.target.value);
+    };
+
+    // Function to handle form submission
+    const handleSubmit = async (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+        try {
+            // Send a POST request to the /set_data_path endpoint with the data path
+            const response = await axios.post('http://127.0.0.1:5000/set_data_path', { data_path: dataPath });
+            setMessage(response.data.message); // Set the success message from the server
+            setError(''); // Clear any previous error message
+        } catch (error) {
+            setMessage(''); // Clear any previous success message
+            setError('Error setting data path. Please try again.'); // Set the error message
+        }
+    };
+
+    return (
+        <div>
+            <h1>Data Path Configuration</h1>
+            <p>Please enter the path to your data:</p>
+            {/* Form for entering the data path */}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={dataPath}
+                    onChange={handleInputChange}
+                    placeholder="Enter data path"
+                />
+                <button type="submit">Set Data Path</button>
+            </form>
+            {/* Display success message if available */}
+            {message && <p>{message}</p>}
+            {/* Display error message if available */}
+            {error && <p>{error}</p>}
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
-export default Welcome;
+export default DataPathForm;
