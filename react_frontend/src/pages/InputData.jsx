b2e96ec2df0d880bb26ca4062f2b3855b9cb94a5
Brenda <blore005@ucr.edu>
2024-03-21 11:13:50 +0000

    inputData changes

--- react_frontend/src/pages/InputData.jsx
+++ react_frontend/src/pages/InputData.jsx
@@ -19,36 +19,38 @@ const DataPathForm = () => {
             const response = await axios.post('http://127.0.0.1:5000/set_data_path', { data_path: dataPath });
             setMessage(response.data.message);
             setError('');
-    
+
             // Call generate_plots endpoint after setting the data path
             await axios.get('http://127.0.0.1:5000/api/generate_plots');
-    
+
             navigate('/home'); // Use navigate to go to the /home route
         } catch (error) {
             setMessage('');
-            if (error.response && error.response.data && error.response.data.error) {
-                setError(error.response.data.error); // Set the error message from the backend response
+            if (error.response && error.response.data && error.response.data.error === 'No subjects') {
+                setError('Specified directory does not contain any subject folders.'); // Set the specific error message
             } else {
-                setError('An unexpected error occurred.'); // Set a generic error message
+                setError('Specified directory does not exist.'); // Set the generic error message
             }
         }
     };
-    
+
     return (
-        <div className="inputdata">
-            <h1>Data Path Configuration</h1>
-            <p>Please enter the path to your data:</p>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={dataPath}
-                    onChange={handleInputChange}
-                    placeholder="Enter data path"
-                />
-                <button type="submit">Set Data Path</button>
-            </form>
-            {message && <p>{message}</p>}
-            {error && <p>{error}</p>}
+        <div class="box">
+            <div className="inputdata">
+                <h1>Data Path Configuration</h1>
+                <p>Please enter the path to your data:</p>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={dataPath}
+                        onChange={handleInputChange}
+                        placeholder="Enter data path"
+                    />
+                    <button type="submit">Set Data Path</button>
+                </form>
+                {message && <p>{message}</p>}
+                {error && <p>{error}</p>}
+            </div>
         </div>
     );
 };
