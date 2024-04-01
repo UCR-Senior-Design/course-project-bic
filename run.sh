d46504cd9155791e7544d53a23027340f3ae8bd8
Brenda <blore005@ucr.edu>
2024-04-01 11:11:22 +0000

    updated run.sh file

--- run.sh
+++ run.sh
@@ -4,35 +4,20 @@
 cleanup() {
     echo "Exiting..."
     # Kill the background processes
-    kill "$pid_http_server" "$pid_images" >/dev/null 2>&1
+    kill "$pid_images" >/dev/null 2>&1
     exit 1
 }
 
 # Trap interrupt signal (Ctrl+C)
 trap cleanup SIGINT
 
-# Run http server to serve images
-cd /home/blore005/data/derivatives
-
-python3 -m http.server --bind 127.0.0.1 8080 &
-
-
-
-# Store the PID of the background process
-pid_http_server=$!
-
-# Run images.py in the background
-
 python3 /home/blore005/course-project-bic/flask_backend/app/images.py &
-
 
 # Store the PID of the background process
 pid_images=$!
 
 # Run npm dev server
-
 cd /home/blore005/course-project-bic/react_frontend
-
 npm run dev
 
 # If npm run dev exits, kill the background processes
